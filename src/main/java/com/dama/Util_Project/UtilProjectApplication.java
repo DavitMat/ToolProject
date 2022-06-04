b453d13567d53ecdaec5e3dc8bebea3985089a75
Davit Matevosyan <davitmatevosyan@mail.com>
2022-06-04 13:20:40 +0400

    Initial commit of the project

---
+++ src/main/java/com/dama/Util_Project/UtilProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dama.Util_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dama/Util_Project/UtilProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dama.Util_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
